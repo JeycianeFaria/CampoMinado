cc99c09f1074ba1d3f9e97a1834bc5f56da9d931
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-02 22:30:41 -0300

    Adicionar classe Campo

---
+++ src/main/java/com/example/campominado/model/Campo.java
@@ -0,0 +1,137 @@
+package com.example.campominado.model;
+
+import com.example.campominado.exceptions.ExplosaoException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+    private final int linha;
+    private final int coluna;
+
+    private boolean minado = false;
+    private boolean aberto = false;
+    private boolean marcado = false;
+
+    private List<Campo> vizinhos = new ArrayList<>();
+
+    Campo(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+
+    public boolean isMarcado() {
+        return marcado;
+    }
+
+    public boolean isMinado() {
+        return minado;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    boolean objetivoAlcancado(){
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+
+        return desvendado || protegido;
+    }
+
+    long minasNaVizinhaca(){
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar(){
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+
+    boolean adicionarVizinho(Campo vizinho) {
+        boolean linhaDiferente = linha != vizinho.linha;
+        boolean colunaDiferente = coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferente && colunaDiferente;
+
+        int deltaLinha = Math.abs(linha - vizinho.linha);
+        int deltaColuna = Math.abs(coluna - vizinho.coluna);
+        int deltaGeral = deltaLinha + deltaColuna;
+
+        if (deltaGeral == 1 && !diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else if (deltaGeral == 2 && diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    boolean abrir() {
+
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    boolean vizinhancaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+
+    void alternarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+
+    void minar(){
+        minado = true;
+    }
+
+    public String toString() {
+        if (marcado){
+            return "x";
+        } else if (aberto && minado) {
+            return "*";
+        } else if (aberto && minasNaVizinhaca() > 0){
+            return Long.toString(minasNaVizinhaca());   //TODO: tem como adicionar cor no java pelo terminar,pesquisar e implantar.
+        }else if (aberto){
+            return " ";
+        }else {
+            return "?";
+        }
+    }
+}
