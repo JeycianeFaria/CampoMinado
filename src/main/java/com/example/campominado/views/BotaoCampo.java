d5ac79ac9937c8e666261ac4d0f9a80772505e81
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-06 17:16:47 -0300

    :construction: feat: Configurar estilo dos botões

--- src/main/java/com/example/campominado/views/BotaoCampo.java
+++ src/main/java/com/example/campominado/views/BotaoCampo.java
@@ -5,10 +5,24 @@ import com.example.campominado.model.CampoEvento;
 import com.example.campominado.model.CampoObservador;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BotaoCampo extends JButton implements CampoObservador {
 
+    private final Color BG_PADRAO = new Color(184,184,184);
+    private final Color BG_MARCAR = new Color(8,179,247);
+    private final Color BG_EXPLODIR = new Color(189,66,68);
+    private final Color TEXTO_VERDE = new Color(0,100,0);
+
+    private Campo campo;
+
+
     public BotaoCampo(Campo campo) {
+
+        this.campo = campo;
+
+        setBorder(BorderFactory.createBevelBorder(0));
+        setBackground(BG_PADRAO);
 
         campo.registrarObservadores(this);
 
