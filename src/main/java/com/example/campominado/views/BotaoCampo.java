7232f8735e86fa4d0f3fe3875781762a19ed6172
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-06 16:55:11 -0300

    :sparkles: feat: Implementar Campo Observador ao Botao campo e implementar evento ocoreu

--- src/main/java/com/example/campominado/views/BotaoCampo.java
+++ src/main/java/com/example/campominado/views/BotaoCampo.java
@@ -1,14 +1,29 @@
 package com.example.campominado.views;
 
 import com.example.campominado.model.Campo;
+import com.example.campominado.model.CampoEvento;
+import com.example.campominado.model.CampoObservador;
 
 import javax.swing.*;
 
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
 
     public BotaoCampo(Campo campo) {
 
     }
 
+    @Override
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+        switch (evento){
+            case ABRIR:
+                break;
+            case MARCAR:
+                break;
+            case EXPLODIR:
+                break;
+            default:
+                break;
+        }
+    }
 
 }
