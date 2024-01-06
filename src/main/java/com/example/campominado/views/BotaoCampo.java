55f79db77e04c080928b81346a320cfa3a034f88
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-06 16:53:48 -0300

    :sparkles: feat: Criar classe Botao Campo

---
+++ src/main/java/com/example/campominado/views/BotaoCampo.java
@@ -0,0 +1,14 @@
+package com.example.campominado.views;
+
+import com.example.campominado.model.Campo;
+
+import javax.swing.*;
+
+public class BotaoCampo extends JButton {
+
+    public BotaoCampo(Campo campo) {
+
+    }
+
+
+}
