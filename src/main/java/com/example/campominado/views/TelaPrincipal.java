e2f6197487b39f4760704a9093bd3febccc74f0d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-06 16:11:15 -0300

    :sparkles: feat: Criar classe Tela Principal

---
+++ src/main/java/com/example/campominado/views/TelaPrincipal.java
@@ -0,0 +1,27 @@
+package com.example.campominado.views;
+
+import com.example.campominado.model.Tabuleiro;
+
+import javax.swing.*;
+
+public class TelaPrincipal extends JFrame {
+
+    public TelaPrincipal() {
+
+        Tabuleiro tabuleiro = new Tabuleiro(16,30,50);
+
+        setTitle("Campo Minado");
+        setSize(690,438);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+    }
+
+    public static void main(String[] args) {
+        new TelaPrincipal();
+    }
+
+}
