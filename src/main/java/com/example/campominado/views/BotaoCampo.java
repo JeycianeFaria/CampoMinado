c13ec240d2b07c24640282669754027e944537f6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-07 15:53:43 -0300

    :construction: feat: Implementar interface MouseListener

--- src/main/java/com/example/campominado/views/BotaoCampo.java
+++ src/main/java/com/example/campominado/views/BotaoCampo.java
@@ -6,13 +6,15 @@ import com.example.campominado.model.CampoObservador;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class BotaoCampo extends JButton implements CampoObservador {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener {
 
-    private final Color BG_PADRAO = new Color(184,184,184);
-    private final Color BG_MARCAR = new Color(8,179,247);
-    private final Color BG_EXPLODIR = new Color(189,66,68);
-    private final Color TEXTO_VERDE = new Color(0,100,0);
+    private final Color BG_PADRAO = new Color(184, 184, 184);
+    private final Color BG_MARCAR = new Color(8, 179, 247);
+    private final Color BG_EXPLODIR = new Color(189, 66, 68);
+    private final Color TEXTO_VERDE = new Color(0, 100, 0);
 
     private Campo campo;
 
@@ -24,13 +26,15 @@ public class BotaoCampo extends JButton implements CampoObservador {
         setBorder(BorderFactory.createBevelBorder(0));
         setBackground(BG_PADRAO);
 
+        addMouseListener(this);
+
         campo.registrarObservadores(this);
 
     }
 
     @Override
     public void eventoOcorreu(Campo campo, CampoEvento evento) {
-        switch (evento){
+        switch (evento) {
             case ABRIR:
                 aplicarEstiloAbrir();
                 break;
@@ -56,6 +60,24 @@ public class BotaoCampo extends JButton implements CampoObservador {
     }
 
     private void aplicarEstiloAbrir() {
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
     }
 
 }
