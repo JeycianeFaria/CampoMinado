a2e33a1fc83b5e6625f9c56215b72f65ec8cfd5d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-17 21:05:20 -0300

    :ok_hand: style: organizar metodos e dados da classe Tabuleiro

--- src/main/java/com/example/campominado/model/Tabuleiro.java
+++ src/main/java/com/example/campominado/model/Tabuleiro.java
@@ -10,7 +10,6 @@ public class Tabuleiro implements CampoObservador {
     private final int linhas;
     private final int colunas;
     private final int minas;
-
     private final List<Campo> campos = new ArrayList<>();
     private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
 
@@ -26,25 +25,13 @@ public class Tabuleiro implements CampoObservador {
 
     }
 
+
     public int getLinhas() {
         return linhas;
     }
 
     public int getColunas() {
         return colunas;
-    }
-
-    public void paraCadaCampo(Consumer<Campo> funcao) {
-        campos.forEach(funcao);
-    }
-
-    public void registrarObservadores(Consumer<ResultadoEvento> observador) {
-        observadores.add(observador);
-    }
-
-    private void notificarObservadores(boolean resultado) {
-        observadores.stream()
-                .forEach(o -> o.accept(new ResultadoEvento(resultado)));
     }
 
     public Campo getCampo(int linha, int coluna) {
@@ -60,6 +47,18 @@ public class Tabuleiro implements CampoObservador {
         return campoOptional.get();
     }
 
+
+    public boolean objetivoAlcancado() {
+        return campos.stream().allMatch(c -> c.objetivoAlcancado());
+    }
+
+    public void paraCadaCampo(Consumer<Campo> funcao) {
+        campos.forEach(funcao);
+    }
+
+    public void registrarObservadores(Consumer<ResultadoEvento> observador) {
+        observadores.add(observador);
+    }
 
     public void abrirCampo(int linha, int coluna) {
 
@@ -84,6 +83,26 @@ public class Tabuleiro implements CampoObservador {
                 .ifPresent(c -> c.alternarMarcacao());
     }
 
+    public void reiniciar() {
+        campos.stream().forEach(c -> c.reiniciar());
+        sortearAsMinas();
+    }
+
+    private void sortearAsMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = c -> c.isMinado();
+
+        do {
+
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
+
+        } while (minasArmadas < minas);
+
+
+    }
+
     private void gerarCampos() {
 
         for (int linha = 0; linha < linhas; linha++) {
@@ -106,29 +125,11 @@ public class Tabuleiro implements CampoObservador {
         }
     }
 
-    private void sortearAsMinas() {
-        long minasArmadas = 0;
-        Predicate<Campo> minado = c -> c.isMinado();
-
-        do {
-
-            int aleatorio = (int) (Math.random() * campos.size());
-            campos.get(aleatorio).minar();
-            minasArmadas = campos.stream().filter(minado).count();
-
-        } while (minasArmadas < minas);
-
-
+    private void notificarObservadores(boolean resultado) {
+        observadores.stream()
+                .forEach(o -> o.accept(new ResultadoEvento(resultado)));
     }
 
-    public boolean objetivoAlcancado() {
-        return campos.stream().allMatch(c -> c.objetivoAlcancado());
-    }
-
-    public void reiniciar() {
-        campos.stream().forEach(c -> c.reiniciar());
-        sortearAsMinas();
-    }
 
     @Override
     public void eventoOcorreu(Campo campo, CampoEvento evento) {
