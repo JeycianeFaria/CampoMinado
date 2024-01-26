1aba170e1cd4fe64012c7540db7826a22c64a6d5
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-26 17:03:02 -0300

    :ok_hand: style: ajustar identação da classe Resultado Evento

--- src/main/java/com/example/campominado/model/ResultadoEvento.java
+++ src/main/java/com/example/campominado/model/ResultadoEvento.java
@@ -4,9 +4,11 @@ public class ResultadoEvento {
 
     private final boolean ganhou;
 
+
     public ResultadoEvento(boolean ganhou) {
         this.ganhou = ganhou;
     }
+
 
     public boolean isGanhou() {
         return ganhou;
